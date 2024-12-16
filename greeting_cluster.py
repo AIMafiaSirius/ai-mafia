e1af32edabecebabd51c9a0f31b4de05ac9d88a9
Subio <subikos@mail.ru>
2024-12-16 15:59:21 +0300

    Getting cluster v0.35

--- greeting_cluster.py
+++ greeting_cluster.py
@@ -8,10 +8,9 @@ from chatsky import (
     conditions as cnd,
     responses as rsp,
     destinations as dst,
+    BaseCondition,
     BaseResponse,
     MessageInitTypes,
-    AnyResponse,
-    AbsoluteNodeLabel,
 )
 from chatsky.processing import ModifyResponse
 import random
@@ -37,6 +36,8 @@ class NewRoom(BaseResponse):
 
 class RandomID(ModifyResponse):
     async def modified_response(self, _: BaseResponse, __: Context) -> MessageInitTypes:
+        if len(map_room) == 0:
+            return "К сожалению сейчас нет открытых игр, попробуйте позже или создайте свою"
         curId, name = random.choice(map_room)
         return "Id: " + str(curId) + '\n' + "Комната: " + str(name) + '\n' + "Присоединиться?"
 
@@ -44,29 +45,31 @@ class RandomID(ModifyResponse):
 greeting_script = {
     "greeting_flow": {
         "start_node": {
-            TRANSITIONS: [Tr(dst="greeting_node", cnd=cnd.ExactMatch("/start"))]
-            # start node handles the initial handshake (command /start)
+            TRANSITIONS: [Tr(dst="greeting_node", cnd=cnd.ExactMatch("/start"))],
         },
         "greeting_node": {
             RESPONSE: "Привет! Нужна ли вам инструкция по игре?",
-            TRANSITIONS: [Tr(dst=("game_start_node"), cnd=cnd.ExactMatch("Да"))],
-            TRANSITIONS: [Tr(dst=("room_flow", "choose"), cnd=cnd.ExactMatch("Нет"))],
+            TRANSITIONS: [
+                Tr(dst=("instruction"), cnd=cnd.ExactMatch("Да")),
+                Tr(dst=("to_room_flow", "choose"), cnd=cnd.ExactMatch("Нет")),
+            ],
         },
         "instruction": {
-            # RESPONSE: "...",
-            TRANSITIONS: [Tr(dst=("room_flow", "choose"))],
+            RESPONSE: "...",
+            TRANSITIONS: [Tr(dst=("to_room_flow", "choose"))],
         },
         "fallback_node": {
             RESPONSE: "К сожалению я не могу обработать такую команду, введите другую",
-            TRANSITIONS: [Tr(dst=dst.Backward())]
-            # this transition is unconditional
+            TRANSITIONS: [Tr(dst=dst.Backward())],
         },
     },
-    "room_flow": {
+    "to_room_flow": {
         "choose": {
             RESPONSE: "Создай комнату или присоединись к существующей",
-            TRANSITIONS: [Tr(dst=("enter_id"), cnd=cnd.ExactMatch("Присоединиться"))],
-            TRANSITIONS: [Tr(dst=("make"), cnd=cnd.ExactMatch("Создать"))],
+            TRANSITIONS: [
+                Tr(dst=("enter_id"), cnd=cnd.ExactMatch("Присоединиться")),
+                Tr(dst=("make"), cnd=cnd.ExactMatch("Создать")),
+            ],
         },
         "make": {
             RESPONSE: "Введите название для комнаты",
@@ -74,17 +77,28 @@ greeting_script = {
         },
         "new": {
             RESPONSE: NewRoom(),
-            TRANSITIONS: [Tr(dst=("choose"), cnd=cnd.ExactMatch("Назад"))],
-            # TRANSITIONS: [Tr(dst=(...), cnd=cnd.ExactMatch("Да"))],
+            TRANSITIONS: [
+                Tr(dst=("choose"), cnd=cnd.ExactMatch("Назад")),
+                Tr(dst=("in_room_flow", "ready"), cnd=cnd.ExactMatch("Да")),
+            ],
         },
         "enter_id": {
             RESPONSE: "Введите id комнаты, или присоединитесь к случайной",
-            TRANSITIONS: [Tr(dst=("random_id"), cnd=cnd.ExactMatch("К случайной"))],
-            # TRANSITIONS: [Tr(dst(""), )],
+            TRANSITIONS: [
+                Tr(dst=("random_id"), cnd=cnd.ExactMatch("К случайной")),
+            ],
         },
         "random_id": {
             PRE_RESPONSE: {"random_id_service": RandomID()},
             RESPONSE: "",
+            TRANSITIONS: [
+                Tr(dst=("in_room_flow", "ready"), cnd=cnd.ExactMatch("Да")),
+            ],
+        },
+    },
+    "in_room_flow": {
+        "ready": {
+            RESPONSE: "Нажмите готов для подтверждения игры",
         },
     },
 }
