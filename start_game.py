103a002ed3d488d0086f9fd27b94c170d45b7f77
Mike <misha.barantsev@gmail.com>
2024-12-16 18:12:19 +0300

    update random

--- start_game.py
+++ start_game.py
@@ -1,73 +1,69 @@
-import chatsky.conditions as cnd
-import chatsky.destinations as dst
-from chatsky import RESPONSE, TRANSITIONS, Pipeline, PRE_RESPONSE
-from chatsky import Transition as Tr
 import random
+from enum import Enum
+
+import chatsky.conditions as cnd
+from chatsky import PRE_TRANSITION, RESPONSE, TRANSITIONS, BaseProcessing, BaseResponse, Context, Pipeline
+from chatsky import Transition as Tr
 
 
-players = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-players_id = []
-# массив used нужны id телеграммов игроков
-
-mafia_id = []
-# id телегоаммов мафии
-
-mafia = []
-# номера мафии
-
-cards = {0: "Мирный",
-         1: "Комиссар",
-         2: "Мафия",
-         3: "Дон мафии"}
-# карты в int
-
-players_role = [2, 2, 3, 1, 0, 0, 0, 0, 0, 0]
-# роли игкоков
-
-
-def mixed_role():
+def mixed_role(players_role):
     for i in range(len(players_role)):
         r = random.randint(0, i)
         players_role[i], players_role[r] = players_role[r], players_role[i]
     return players_role
 
-
-def get_role_for_all():
-    role = ""
-    for i in range(10):
-        if i != 9:
-            role = role + "Ваша роль: " + cards[players_role[i]] + "\n"
-        else:
-            role = role + "Ваша роль: " + cards[players_role[i]]
-
-    return role
+class Role(Enum):
+    peaceful = "мирный"
+    commissar = "комиссар"
+    mafia = "мафия"
+    don = "дон"
 
 
+now_role = ["комиссар", "мафия", "мафия", "дон", "мирный", "мирный", "мирный", "мирный", "мирный", "мирный"]
 
-number_of_living_players = len(players)
+now_role = mixed_role(now_role)
 
-players_role = mixed_role()
+class AssignRoles(BaseProcessing):
+    async def call(self, ctx: Context):
+        # retrieve by room's id from database
+        ctx.misc["players_id"] = [
+            "id_1",
+            "id_2",
+            "id_3",
+            "id_4",
+            "id_5",
+            "id_6",
+            "id_7",
+            "id_8",
+            "id_9",
+            "id_10",
+        ]
+
+        # retrieve roles from database if exist
+        # else initialize
+
+        ctx.misc["players_roles"] = now_role
+
+
+class RoleResponse(BaseResponse):
+    async def call(self, ctx: Context):
+        "Начало игры, раздача карт!"
+        current_user_role: Role = ctx.misc["players_roles"][1] # should be `ctx.id`
+        return current_user_role
 
 mafia_get_role = {
     "greeting_flow": {
         "start_node": {
-            TRANSITIONS: [Tr(dst="greeting_node", cnd=cnd.ExactMatch("/start"))]
-
+            TRANSITIONS: [Tr(dst="greeting_node", cnd=cnd.ExactMatch("get_role"))],
+            PRE_TRANSITION: {"assign_roles": AssignRoles()},
         },
         "greeting_node": {
-            RESPONSE: "Начало игры, раздача карт!",
-            TRANSITIONS: [Tr(dst=("mafia_get_role_flow", "get_role1"))],
+            RESPONSE: RoleResponse(),
         },
         "fallback_node": {
             RESPONSE: "That was against the rules",
             TRANSITIONS: [Tr(dst="greeting_node")],
             # не правильный текст
-        },
-    },
-    "mafia_get_role_flow": {
-        "get_role1": {
-            RESPONSE: get_role_for_all(),
         },
     },
 }
