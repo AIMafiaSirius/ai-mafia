b7053bb471d03a6a58c591249a57c22bbbe68ff6
Mike <misha.barantsev@gmail.com>
2024-12-18 15:12:21 +0300

    раздача карт без базы данных/ соит мой id

--- start_game.py
+++ start_game.py
@@ -3,57 +3,33 @@ from random import randint
 from typing import TYPE_CHECKING
 
 import chatsky.conditions as cnd
-from chatsky import RESPONSE, TRANSITIONS, BaseResponse, Context, Pipeline
+import chatsky.destinations as dst
+from chatsky import PRE_TRANSITION, RESPONSE, TRANSITIONS, BaseProcessing, BaseResponse, Context, Pipeline
 from chatsky import Transition as Tr
 from chatsky.messengers.telegram import LongpollingInterface
+from dotenv import load_dotenv
 
+from ai_mafia.db.routines import add_user, find_user, increment_counter
 from bson.objectid import ObjectId
-from pydantic import BaseModel, ConfigDict, Field
 
+if TYPE_CHECKING:
+    import telegram as tg
 
-class UserModel(BaseModel):
-    """Data model for storing info about user."""
+    from ai_mafia.db.models import Player, UserModel
+load_dotenv()
 
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    db_id: ObjectId | None = Field(default=None, alias="_id", description="Unique identifier in mongo db")
-
-    tg_id: int
-    """User's telegram id"""
-
-    tg_nickname: str
-    """User's telegram nickname"""
-
-    room_id: ObjectId | None
-    """Id of a room there user currently in"""
-
-    win_counter: int = 0
-    """Total number of wins from this user from all his sessions."""
-
-    game_counter: int = 0
-    """Total number of played games from this user from all his sessions."""
 
 
 class Player:
-    user_num: int
+    user_num: int = -1
 
     role: str | None = None
 
     is_alive: bool = True
 
 
-players = [
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-    "976818136",
-]
+players = ["976818136", "976818136", "976818136", "976818136", "976818136", "976818136", "976818136", "976818136", 
+           "976818136", "976818136"]
 players_role = ["комиссар", "мафия", "мафия", "дон", "мирный", "мирный", "мирный", "мирный", "мирный", "мирный"]
 
 gamers = {}
@@ -66,22 +42,22 @@ def mix(arr):
 
 def get_role_for_all():
     for i in range(10):
-        gamers[players[i]] = Player()
-        gamers[players[i]].user_num = i
-        gamers[players[i]].role = players_role[i]
-        gamers[players[i]].is_alive = True
+        cur = Player()
+        cur.is_alive = True
+        cur.user_num = i
+        cur.role = players_role[i]
 
+        gamers[players[i]] = cur
 
 mix(players)
 mix(players_role)
 get_role_for_all()
 
-
 class GetCards(BaseResponse):
     async def call(self, ctx: Context):
-        players_info: UserModel = ctx.misc["user_info"]
-        get_id = players_info.tg_id
-        return f"Игрок номер {gamers[get_id].number} ваша карта: {gamers[get_id].role}"
+        #players_info: UserModel = ctx.misc["user_info"]
+        get_id = "976818136"
+        return f"Игрок номер {gamers[get_id].user_num + 1} ваша карта: {gamers[get_id].role}"
 
 
 get_card_script = {
